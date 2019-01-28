0a80abd3ef31c5b5aebe6f779ccebe1bc82ad11b
Vojtech Balak <balakvojtech@gmail.com>
2019-01-28 18:57:15 +0100

    Switched from pathogen to vim-plug

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p ~/dotfiles_old/.config
 
 cd ~/dotfiles
 
-BLACKLIST="-I install.sh -I .config -I .git -I .gitignore -I .gitmodules -I README.md -I screenshot.png -I st"
+BLACKLIST="-I install.sh -I .config -I .git -I .gitignore -I .gitmodules -I README.md -I screenshot.png"
 
 for file in `ls -A $BLACKLIST`
 do
@@ -20,4 +20,4 @@ do
     ln -s ~/dotfiles/.config/$file ~/.config/$file
 done
 
-git submodule update --init --recursive
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
