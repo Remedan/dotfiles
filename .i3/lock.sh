d1debe1f542708f27557a2a50ef6ad5e124d13f3
Vojtech Balak <balakvojtech@gmail.com>
2019-10-02 13:50:01 +0200

    Switched sound control from alsa to pulseaudio

--- .i3/lock.sh
+++ .i3/lock.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-SOUND=$(amixer sget Master | awk -F"[][]" '/Front Left.*%/ { print $4 }')
-amixer -q set Master mute
+MUTED=$(pacmd list-sinks | awk '/muted/ { print $2 }')
+pactl set-sink-mute @DEFAULT_SINK@ 1
 scrot /tmp/screenshot.png
 convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -n -i /tmp/screenshotblur.png && if [ $SOUND = "on" ]; then amixer -q set Master unmute; fi
+rm /tmp/screenshot.png
+i3lock -n -i /tmp/screenshotblur.png && if [[ $MUTED != "yes" ]]; then pactl set-sink-mute @DEFAULT_SINK@ 0; fi
