3288c3a29080ebb99d5ae058b2c3ebefb8baa62c
Vojtech Balak <vojtech@balak.me>
2021-03-21 18:27:04 +0100

    Fix typo

--- install.sh
+++ install.sh
@@ -19,5 +19,5 @@ do
     ln -s "$HOME/dotfiles/.config/$file" "$HOME/.config/$file"
 done
 
-curl -fLo "$HOME/.config/nvin/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo "$HOME/.config/nvim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall
