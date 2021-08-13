51d14021506eb1fd2ab7161a6520bafc341c4c2a
Vojtěch Balák <vojtech@balak.me>
2021-08-13 11:37:33 +0200

    Create custom.el if it doesn't exist

--- dotfiles/config/emacs/init.el
+++ dotfiles/config/emacs/init.el
@@ -41,6 +41,8 @@
 
 ;; Separate config for customize
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
 (load custom-file)
 
 ;; Key mapping
