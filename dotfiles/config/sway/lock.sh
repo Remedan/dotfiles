00e6c68fcc44603cfcd4a3bc8006387259879561
Vojtěch Balák <vojtech@balak.me>
2021-08-13 10:31:22 +0200

    Remove sway and waybar

--- dotfiles/config/sway/lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-MUTED=$(pacmd list-sinks | awk '/muted/ { print $2 }')
-pactl set-sink-mute @DEFAULT_SINK@ 1
-grim /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-rm /tmp/screenshot.png
-swaylock -n -i /tmp/screenshotblur.png && if [[ $MUTED != "yes" ]]; then pactl set-sink-mute @DEFAULT_SINK@ 0; fi
