1d90f525b04662f95412d6a113edc3ce8f20c82e
Vojtěch Balák <vojtech@balak.me>
2021-08-24 10:18:59 +0200

    Add tree-sitter

--- dotfiles/config/emacs/init.el
+++ dotfiles/config/emacs/init.el
@@ -162,6 +162,12 @@
 (use-package which-key
   :config (which-key-mode))
 
+;; Tree-sitter
+(use-package tree-sitter)
+(use-package tree-sitter-langs)
+(global-tree-sitter-mode)
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+
 ;; Language-specific
 
 ;; LSP
