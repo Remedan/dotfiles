15fb583de2f8466c6e325f151ff4bd83f99bf6d4
Vojtěch Balák <vojtech@balak.me>
2023-04-12 21:25:54 +0200

    Add keyboard config

--- modules/common.nix
+++ modules/common.nix
@@ -1,4 +1,13 @@
 {
-  home.stateVersion = "23.05";
+  home = {
+    stateVersion = "23.05";
+    keyboard = {
+      layout = "us,cz(qwerty)";
+      options = [
+        "grp:alt_shift_toggle"
+        "caps:escape"
+      ];
+    };
+  };
   programs.home-manager.enable = true;
 }
