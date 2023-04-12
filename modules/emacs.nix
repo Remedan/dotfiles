e64b646a691f40b1a4d5d5de1ed9854b63aab447
Vojtěch Balák <vojtech@balak.me>
2023-04-12 22:08:34 +0200

    Enable the Emacs service

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -3,4 +3,5 @@
     ".config/emacs/init.el".source = ../dotfiles/config/emacs/init.el;
     ".config/emacs/sakamoto.png".source = ../dotfiles/config/emacs/sakamoto.png;
   };
+  services.emacs.enable = true;
 }
