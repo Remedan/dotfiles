079b09ee29be9fefa5330b399f05576cf4e277ad
Vojtěch Balák <vojtech@balak.me>
2023-05-30 10:48:28 +0200

    Set formatter and format the code

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -3,52 +3,52 @@ let
   colorschemes = {
     "gruvbox-dark" = {
       primary = {
-        background        = "#282828";
-        foreground        = "#ebdbb2";
+        background = "#282828";
+        foreground = "#ebdbb2";
         bright_foreground = "#fbf1c7";
-        dim_foreground    = "#a89984";
+        dim_foreground = "#a89984";
       };
       cursor = {
-        text   = "CellBackground";
+        text = "CellBackground";
         cursor = "CellForeground";
       };
       vi_mode_cursor = {
-        text   = "CellBackground";
+        text = "CellBackground";
         cursor = "CellForeground";
       };
       selection = {
-        text       = "CellBackground";
+        text = "CellBackground";
         background = "CellForeground";
       };
       bright = {
-        black   = "#928374";
-        red     = "#fb4934";
-        green   = "#b8bb26";
-        yellow  = "#fabd2f";
-        blue    = "#83a598";
+        black = "#928374";
+        red = "#fb4934";
+        green = "#b8bb26";
+        yellow = "#fabd2f";
+        blue = "#83a598";
         magenta = "#d3869b";
-        cyan    = "#8ec07c";
-        white   = "#ebdbb2";
+        cyan = "#8ec07c";
+        white = "#ebdbb2";
       };
       normal = {
-        black   = "#665c54";
-        red     = "#cc241d";
-        green   = "#98971a";
-        yellow  = "#d79921";
-        blue    = "#458588";
+        black = "#665c54";
+        red = "#cc241d";
+        green = "#98971a";
+        yellow = "#d79921";
+        blue = "#458588";
         magenta = "#b16286";
-        cyan    = "#689d6a";
-        white   = "#a89984";
+        cyan = "#689d6a";
+        white = "#a89984";
       };
       dim = {
-        black   = "#32302f";
-        red     = "#9d0006";
-        green   = "#79740e";
-        yellow  = "#b57614";
-        blue    = "#076678";
+        black = "#32302f";
+        red = "#9d0006";
+        green = "#79740e";
+        yellow = "#b57614";
+        blue = "#076678";
         magenta = "#8f3f71";
-        cyan    = "#427b58";
-        white   = "#928374";
+        cyan = "#427b58";
+        white = "#928374";
       };
     };
     "selenized-dark" = {
@@ -57,24 +57,24 @@ let
         foreground = "0xadbcbc";
       };
       normal = {
-        black   = "0x184956";
-        red     = "0xfa5750";
-        green   = "0x75b938";
-        yellow  = "0xdbb32d";
-        blue    = "0x4695f7";
+        black = "0x184956";
+        red = "0xfa5750";
+        green = "0x75b938";
+        yellow = "0xdbb32d";
+        blue = "0x4695f7";
         magenta = "0xf275be";
-        cyan    = "0x41c7b9";
-        white   = "0x72898f";
+        cyan = "0x41c7b9";
+        white = "0x72898f";
       };
       bright = {
-        black   = "0x2d5b69";
-        red     = "0xff665c";
-        green   = "0x84c747";
-        yellow  = "0xebc13d";
-        blue    = "0x58a3ff";
+        black = "0x2d5b69";
+        red = "0xff665c";
+        green = "0x84c747";
+        yellow = "0xebc13d";
+        blue = "0x58a3ff";
         magenta = "0xff84cd";
-        cyan    = "0x53d6c7";
-        white   = "0xcad8d9";
+        cyan = "0x53d6c7";
+        white = "0xcad8d9";
       };
     };
     "dracula" = {
@@ -114,39 +114,40 @@ let
           background = "0x44475a";
         };
         normal = {
-          black   = "0x000000";
-          red     = "0xff5555";
-          green   = "0x50fa7b";
-          yellow  = "0xf1fa8c";
-          blue    = "0xbd93f9";
+          black = "0x000000";
+          red = "0xff5555";
+          green = "0x50fa7b";
+          yellow = "0xf1fa8c";
+          blue = "0xbd93f9";
           magenta = "0xff79c6";
-          cyan    = "0x8be9fd";
-          white   = "0xbfbfbf";
+          cyan = "0x8be9fd";
+          white = "0xbfbfbf";
         };
         bright = {
-          black   = "0x4d4d4d";
-          red     = "0xff6e67";
-          green   = "0x5af78e";
-          yellow  = "0xf4f99d";
-          blue    = "0xcaa9fa";
+          black = "0x4d4d4d";
+          red = "0xff6e67";
+          green = "0x5af78e";
+          yellow = "0xf4f99d";
+          blue = "0xcaa9fa";
           magenta = "0xff92d0";
-          cyan    = "0x9aedfe";
-          white   = "0xe6e6e6";
+          cyan = "0x9aedfe";
+          white = "0xe6e6e6";
         };
         dim = {
-          black   = "0x14151b";
-          red     = "0xff2222";
-          green   = "0x1ef956";
-          yellow  = "0xebf85b";
-          blue    = "0x4d5b86";
+          black = "0x14151b";
+          red = "0xff2222";
+          green = "0x1ef956";
+          yellow = "0xebf85b";
+          blue = "0x4d5b86";
           magenta = "0xff46b0";
-          cyan    = "0x59dffc";
-          white   = "0xe6e6d1";
+          cyan = "0x59dffc";
+          white = "0xe6e6d1";
         };
       };
     };
   };
-in {
+in
+{
   programs.alacritty = {
     enable = true;
     settings = {
