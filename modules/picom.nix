5da9a036f76b9bea344b0a7e1e8cd8fe142ef965
Vojtěch Balák <vojtech@balak.me>
2023-07-10 15:25:53 +0200

    Add temporary picom workaround by specifying config as text

--- modules/picom.nix
+++ modules/picom.nix
@@ -1,7 +1,78 @@
 { ... }:
 {
+  # TODO services.picom crashes, troubleshoot and remove this
+  home.file.".config/picom/picom.conf".text = ''
+    # Shadow
+    shadow = true;
+    shadow-radius = 5;
+    shadow-offset-x = -5;
+    shadow-offset-y = -5;
+    shadow-opacity = 0.75;
+    shadow-exclude = [
+        "name = 'oneko'",
+        "class_g = 'firefox' && argb",
+        "class_g = 'thunderbird' && argb",
+        "_GTK_FRAME_EXTENTS@:c",
+        "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
+    ];
+    shadow-ignore-shaped = false;
+
+    # Opacity
+    blur-background = true;
+    blur-background-frame = true;
+    blur-background-fixed = true;
+    blur:
+    {
+        method = "dual_kawase";
+        strength = 5;
+    };
+    blur-background-exclude = [
+        "class_g = 'firefox' && argb",
+        "class_g = 'thunderbird' && argb"
+    ];
+    opacity-rule = [
+    "80:class_g = 'Polybar'",
+    "80:class_g = 'Rofi'",
+    "85:class_g = 'Alacritty' && !focused",
+    "95:class_g = 'Alacritty' && focused",
+    "85:class_g = 'kitty' && !focused",
+    "95:class_g = 'kitty' && focused",
+    ];
+
+    # Fading
+    fading = true;
+    fade-delta = 3;
+    no-fading-openclose = false;
+
+    # Wintypes
+    wintypes:
+    {
+        tooltip = { opacity = 0.95; shadow = false; fade = true; focus = true; };
+        #dock = { shadow = false; };
+        dnd = { shadow = false; };
+    };
+
+    # Other
+    backend = "glx";
+    mark-wmwin-focused = true;
+    mark-ovredir-focused = true;
+    use-ewmh-active-win = true;
+    detect-rounded-corners = true;
+    detect-client-opacity = true;
+    vsync = false;
+    unredir-if-possible = true;
+    detect-transient = true;
+    detect-client-leader = true;
+
+    # GLX backend
+    glx-no-stencil = true;
+    glx-copy-from-front = false;
+  '';
+
+  # TODO services.picom crashes, troubleshoot and re-enable
   services.picom = {
-    enable = true;
+    enable = false;
+
     settings = {
       # Shadow
       shadow = true;
