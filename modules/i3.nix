8c4986a0f95f4ebf7e52bdd5e985705d0a2e87f5
Vojtěch Balák <vojtech@balak.me>
2023-08-07 21:35:08 +0200

    Add i3 colorschemes

--- modules/i3.nix
+++ modules/i3.nix
@@ -1,9 +1,109 @@
-{ lib, terminal, ... }:
+{ lib, colorscheme, terminal, ... }:
 
 let
   # Mod is the Windows key
   mod = "Mod4";
   powerControlMode = "[l]ock log[o]ut [s]uspend [h]ibernate [r]eboot [p]oweroff";
+  colorschemes = {
+    "gruvbox-dark" = {
+      focused = {
+        border = "#676d3d";
+        background = "#676d3d";
+        text = "#ebdbb2";
+        indicator = "#676d3d";
+        childBorder = "#676d3d";
+      };
+      unfocused = {
+        border = "#323232";
+        background = "#323232";
+        text = "#ebdbb2";
+        indicator = "#323232";
+        childBorder = "#323232";
+      };
+      focusedInactive = {
+        border = "#323232";
+        background = "#333333";
+        text = "#ebdbb2";
+        indicator = "#323232";
+        childBorder = "#333333";
+      };
+      urgent = {
+        border = "#383a3b";
+        background = "#383a3b";
+        text = "#ee0000";
+        indicator = "#383a3b";
+        childBorder = "#383a3b";
+      };
+    };
+    "selenized-dark" = {
+      focused = {
+        border = "#75b938";
+        background = "#75b938";
+        text = "#184956";
+        indicator = "#4695f7";
+        childBorder = "#75b938";
+      };
+      unfocused = {
+        border = "#184956";
+        background = "#184956";
+        text = "#adbcbc";
+        indicator = "#72898f";
+        childBorder = "#184956";
+      };
+      focusedInactive = {
+        border = "#41c7b9";
+        background = "#41c7b9";
+        text = "#184956";
+        indicator = "#af88eb";
+        childBorder = "#41c7b9";
+      };
+      urgent = {
+        border = "#dbb32d";
+        background = "#dbb32d";
+        text = "#184956";
+        indicator = "#ed8649";
+        childBorder = "#dbb32d";
+      };
+    };
+    "dracula" = {
+      background = "#F8F8F2";
+      focused = {
+        border = "#6272A4";
+        background = "#6272A4";
+        text = "#F8F8F2";
+        indicator = "#6272A4";
+        childBorder = "#6272A4";
+      };
+      unfocused = {
+        border = "#282A36";
+        background = "#282A36";
+        text = "#BFBFBF";
+        indicator = "#282A36";
+        childBorder = "#282A36";
+      };
+      focusedInactive = {
+        border = "#44475A";
+        background = "#44475A";
+        text = "#F8F8F2";
+        indicator = "#44475A";
+        childBorder = "#44475A";
+      };
+      urgent = {
+        border = "#44475A";
+        background = "#FF5555";
+        text = "#F8F8F2";
+        indicator = "#FF5555";
+        childBorder = "#FF5555";
+      };
+      placeholder = {
+        border = "#282A36";
+        background = "#282A36";
+        text = "#F8F8F2";
+        indicator = "#282A36";
+        childBorder = "#282A36";
+      };
+    };
+  };
 in
 {
   xsession.windowManager.i3 = {
@@ -13,7 +113,7 @@ in
 
       fonts = {
         names = [ "Open Sans" ];
-        size = 11.0;
+        size = 9.0;
       };
 
       window = {
@@ -26,6 +126,8 @@ in
         smartGaps = true;
         smartBorders = "on";
       };
+
+      colors = colorschemes.${colorscheme};
 
       keybindings = lib.mkOptionDefault {
         # Start a terminal
@@ -84,6 +186,12 @@ in
         "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
         "XF86AudioMicMute" = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle";
 
+        # Media keys
+        "XF86AudioPlay" = "exec ~/.config/i3/player-control.sh toggle";
+        "XF86AudioNext" = "exec ~/.config/i3/player-control.sh next";
+        "XF86AudioPrev" = "exec ~/.config/i3/player-control.sh prev";
+        "XF86AudioStop" = "exec ~/.config/i3/player-control.sh stop";
+
         # Sreen brightness controls (brightness up, brightness down)
         "XF86MonBrightnessUp" = "exec xbacklight -inc 5";
         "XF86MonBrightnessDown" = "exec xbacklight -dec 5";
@@ -92,12 +200,6 @@ in
         "Print" = "exec scrot";
         "Shift+Print" = "exec scrot -u";
         "Ctrl+Print" = "exec scrot -s";
-
-        # Media keys
-        "XF86AudioPlay" = "exec ~/.config/i3/player-control.sh toggle";
-        "XF86AudioNext" = "exec ~/.config/i3/player-control.sh next";
-        "XF86AudioPrev" = "exec ~/.config/i3/player-control.sh prev";
-        "XF86AudioStop" = "exec ~/.config/i3/player-control.sh stop";
 
         # Applications
         "${mod}+i" = "exec firefox";
