fb71a31f8311ba612e8705bb59b4d98a52908e9d
Vojtěch Balák <vojtech@balak.me>
2023-08-09 16:40:02 +0200

    Refactor mpd overrides

--- modules/mpd.nix
+++ modules/mpd.nix
@@ -1,21 +1,22 @@
-{ musicDirectory, ... }:
+{ lib, mpdOverrides, ... }:
 {
-  services.mpd = {
-    enable = true;
-    musicDirectory = musicDirectory;
-    extraConfig = ''
-      audio_output {
-          type    "pipewire"
-          name    "PipeWire Sound Server"
-      }
+  services.mpd = lib.recursiveUpdate
+    {
+      enable = true;
+      extraConfig = ''
+        audio_output {
+            type    "pipewire"
+            name    "PipeWire Sound Server"
+        }
 
-      audio_output {
-          type    "fifo"
-          name    "my_fifo"
-          path    "/tmp/mpd.fifo"
-          format  "44100:16:2"
-      }
-    '';
-  };
+        audio_output {
+            type    "fifo"
+            name    "my_fifo"
+            path    "/tmp/mpd.fifo"
+            format  "44100:16:2"
+        }
+      '';
+    }
+    mpdOverrides;
   programs.ncmpcpp.enable = true;
 }
