e3272726d0a568f55d18c3bf6e1dac529135292f
Vojtěch Balák <vojtech@balak.me>
2023-10-12 15:56:34 +0200

    Turn Polybar into a full NixOS module

--- flake.nix
+++ flake.nix
@@ -25,18 +25,6 @@
         extraSpecialArgs = {
           colorscheme = "selenized-dark";
           terminal = "run-alacritty";
-          polybar.override = {
-            services.polybar = {
-              settings = {
-                "bar/b0" = {
-                  monitor = "\${env:MONITOR:DP-0}";
-                };
-                "bar/b1" = {
-                  monitor = "\${env:MONITOR:DP-2}";
-                };
-              };
-            };
-          };
         };
 
         modules = [
@@ -94,6 +82,15 @@
               nixgl.enable = true;
               fonts.enable = true;
               packages.enable = true;
+              polybar = {
+                enable = true;
+                bar0Override = {
+                  monitor = "\${env:MONITOR:DP-0}";
+                };
+                bar1Override = {
+                  monitor = "\${env:MONITOR:DP-2}";
+                };
+              };
             };
           }
           ./user-modules/alacritty.nix
@@ -120,27 +117,6 @@
         extraSpecialArgs = {
           colorscheme = "gruvbox-dark";
           terminal = "WINIT_X11_SCALE_FACTOR=1 run-alacritty";
-          polybar.override = {
-            services.polybar = {
-              settings = {
-                "bar/b0" = {
-                  monitor = "\${env:MONITOR:DP-1-2}";
-                  monitor-fallback = "\${env:MONITOR:eDP-1}";
-                  monitor-strict = true;
-                  font = [
-                    "Symbols\ Nerd\ Font:10;2"
-                    "Open Sans:pixelsize=11;2"
-                    "Source\ Han\ Sans:pixelsize=10;2"
-                  ];
-                  modules-right = "filesystem xkeyboard cpu memory backlight battery date powermenu";
-                };
-                "bar/b1" = {
-                  monitor = "\${env:MONITOR:DP-1-3}";
-                  monitor-strict = true;
-                };
-              };
-            };
-          };
         };
 
         modules = [
@@ -202,6 +178,24 @@
               nixgl.enable = true;
               nodejs.enable = true;
               packages.enable = true;
+              polybar = {
+                enable = true;
+                bar0Override = {
+                  monitor = "\${env:MONITOR:DP-1-2}";
+                  monitor-fallback = "\${env:MONITOR:eDP-1}";
+                  monitor-strict = true;
+                  font = [
+                    "Symbols\ Nerd\ Font:10;2"
+                    "Open Sans:pixelsize=11;2"
+                    "Source\ Han\ Sans:pixelsize=10;2"
+                  ];
+                  modules-right = "filesystem xkeyboard cpu memory backlight battery date powermenu";
+                };
+                bar1Override = {
+                  monitor = "\${env:MONITOR:DP-1-3}";
+                  monitor-strict = true;
+                };
+              };
             };
           }
           ./user-modules/alacritty.nix
--- user-modules/polybar.nix
+++ user-modules/polybar.nix
@@ -1,5 +1,7 @@
-{ lib, pkgs, colorscheme, polybar, ... }:
+{ config, lib, pkgs, ... }:
+with lib;
 let
+  cfg = config.user-modules.polybar;
   colorschemes = {
     "gruvbox-dark" = {
       background = "#282828";
@@ -51,263 +53,274 @@ let
     };
   };
 in
-lib.recursiveUpdate
 {
-  services.polybar = {
-    enable = true;
-    package = pkgs.polybarFull;
-    script = "polybar b0 &";
-    settings = {
-      "colors" = colorschemes.${colorscheme};
-      "settings" = {
-        screenchange-reload = true;
-      };
-      "global/wm" = {
-        margin-top = 5;
-        margin-bottom = 5;
-      };
-      "bar/b0" = {
-        fixed-center = true;
+  options.user-modules.polybar = {
+    enable = mkEnableOption "Polybar";
+    bar0Override = mkOption {
+      type = with types; attrsOf anything;
+      default = { };
+    };
+    bar1Override = mkOption {
+      type = with types; attrsOf anything;
+      default = { };
+    };
+  };
+  config = mkIf cfg.enable {
+    services.polybar = {
+      enable = true;
+      package = pkgs.polybarFull;
+      script = "polybar b0 &";
+      settings = {
+        "colors" = colorschemes.${config.user-modules.common.colorscheme};
+        "settings" = {
+          screenchange-reload = true;
+        };
+        "global/wm" = {
+          margin-top = 5;
+          margin-bottom = 5;
+        };
+        "bar/b0" = {
+          fixed-center = true;
 
-        width = "100%";
-        height = 27;
+          width = "100%";
+          height = 27;
 
-        background = "\${colors.background}";
-        foreground = "\${colors.foreground}";
+          background = "\${colors.background}";
+          foreground = "\${colors.foreground}";
 
-        line-size = 3;
-        line-color = "\${colors.color1}";
+          line-size = 3;
+          line-color = "\${colors.color1}";
 
-        padding-left = 0;
-        padding-right = 2;
-        module-margin = 1;
+          padding-left = 0;
+          padding-right = 2;
+          module-margin = 1;
 
-        font = [
-          "Symbols\ Nerd\ Font:10;2"
-          "Open Sans:pixelsize=11;2"
-          "Source\ Han\ Sans:pixelsize=10;2"
-        ];
+          font = [
+            "Symbols\ Nerd\ Font:10;2"
+            "Open Sans:pixelsize=11;2"
+            "Source\ Han\ Sans:pixelsize=10;2"
+          ];
 
-        modules-left = "i3 playerctl mpd pulseaudio";
-        modules-center = "xwindow";
-        modules-right = "filesystem xkeyboard cpu memory date powermenu";
+          modules-left = "i3 playerctl mpd pulseaudio";
+          modules-center = "xwindow";
+          modules-right = "filesystem xkeyboard cpu memory date powermenu";
 
-        tray-position = "right";
-        tray-padding = 2;
+          tray-position = "right";
+          tray-padding = 2;
 
-        enable-ipc = true;
+          enable-ipc = true;
 
-        scroll-up = "i3wm-wsprev";
-        scroll-down = "i3wm-wsnext";
+          scroll-up = "i3wm-wsprev";
+          scroll-down = "i3wm-wsnext";
 
-        cursor-click = "pointer";
-      };
-      "bar/b1" = {
-        "inherit" = "bar/b0";
-        tray-position = "";
-      };
-      "module/xwindow" = {
-        type = "internal/xwindow";
-        label = "%title:0:40:...%";
-      };
-      "module/xkeyboard" = {
-        type = "internal/xkeyboard";
-        blacklist = [ "num lock" ];
+          cursor-click = "pointer";
+        } // cfg.bar0Override;
+        "bar/b1" = {
+          "inherit" = "bar/b0";
+          tray-position = "";
+        } // cfg.bar1Override;
+        "module/xwindow" = {
+          type = "internal/xwindow";
+          label = "%title:0:40:...%";
+        };
+        "module/xkeyboard" = {
+          type = "internal/xkeyboard";
+          blacklist = [ "num lock" ];
 
-        format-prefix = " ";
-        format-prefix-foreground = "\${colors.color1}";
+          format-prefix = " ";
+          format-prefix-foreground = "\${colors.color1}";
 
-        label-layout = "%layout%";
+          label-layout = "%layout%";
 
-        label-indicator-padding = 2;
-        label-indicator-margin = 1;
-        label-indicator-background = "\${colors.alert}";
-      };
-      "module/filesystem" = {
-        type = "internal/fs";
-        interval = 25;
+          label-indicator-padding = 2;
+          label-indicator-margin = 1;
+          label-indicator-background = "\${colors.alert}";
+        };
+        "module/filesystem" = {
+          type = "internal/fs";
+          interval = 25;
 
-        mount = [ "/" ];
+          mount = [ "/" ];
 
-        label-mounted = "%{F#0a81f5}%mountpoint%%{F-}: %percentage_used%%";
-        label-unmounted = "%mountpoint% not mounted";
-        label-unmounted-foreground = "\${colors.foreground-alt}";
-      };
-      "module/i3" = {
-        type = "internal/i3";
-        format = "<label-state> <label-mode>";
-        index-sort = true;
-        wrapping-scroll = false;
-        enable-scroll = false;
+          label-mounted = "%{F#0a81f5}%mountpoint%%{F-}: %percentage_used%%";
+          label-unmounted = "%mountpoint% not mounted";
+          label-unmounted-foreground = "\${colors.foreground-alt}";
+        };
+        "module/i3" = {
+          type = "internal/i3";
+          format = "<label-state> <label-mode>";
+          index-sort = true;
+          wrapping-scroll = false;
+          enable-scroll = false;
 
-        # Only show workspaces on the same output as the bar
-        pin-workspaces = true;
+          # Only show workspaces on the same output as the bar
+          pin-workspaces = true;
 
-        label-mode-padding = 2;
-        label-mode-foreground = "\${colors.background}";
-        label-mode-background = "\${colors.primary}";
+          label-mode-padding = 2;
+          label-mode-foreground = "\${colors.background}";
+          label-mode-background = "\${colors.primary}";
 
-        # focused = Active workspace on focused monitor
-        label-focused = "%name%";
-        label-focused-underline = "\${colors.primary}";
-        label-focused-padding = 2;
+          # focused = Active workspace on focused monitor
+          label-focused = "%name%";
+          label-focused-underline = "\${colors.primary}";
+          label-focused-padding = 2;
 
-        # unfocused = Inactive workspace on any monitor
-        label-unfocused = "%name%";
-        label-unfocused-padding = 2;
+          # unfocused = Inactive workspace on any monitor
+          label-unfocused = "%name%";
+          label-unfocused-padding = 2;
 
-        # visible = Active workspace on unfocused monitor
-        label-visible = "%name%";
-        label-visible-underline = "\${self.label-focused-underline}";
-        label-visible-padding = "\${self.label-focused-padding}";
+          # visible = Active workspace on unfocused monitor
+          label-visible = "%name%";
+          label-visible-underline = "\${self.label-focused-underline}";
+          label-visible-padding = "\${self.label-focused-padding}";
 
-        # urgent = Workspace with urgency hint set
-        label-urgent = "%name%";
-        label-urgent-background = "\${colors.alert}";
-        label-urgent-padding = 2;
-      };
-      "module/mpd" = {
-        type = "internal/mpd";
-        format-online = "<label-song>  <icon-prev> <icon-stop> <toggle> <icon-next>";
-        format-stopped = "";
+          # urgent = Workspace with urgency hint set
+          label-urgent = "%name%";
+          label-urgent-background = "\${colors.alert}";
+          label-urgent-padding = 2;
+        };
+        "module/mpd" = {
+          type = "internal/mpd";
+          format-online = "<label-song>  <icon-prev> <icon-stop> <toggle> <icon-next>";
+          format-stopped = "";
 
-        icon-prev = "";
-        icon-stop = "";
-        icon-play = "";
-        icon-pause = "";
-        icon-next = "";
+          icon-prev = "";
+          icon-stop = "";
+          icon-play = "";
+          icon-pause = "";
+          icon-next = "";
 
-        label-song-maxlen = 40;
-        label-song-ellipsis = true;
-      };
-      "module/xbacklight" = {
-        type = "internal/xbacklight";
+          label-song-maxlen = 40;
+          label-song-ellipsis = true;
+        };
+        "module/xbacklight" = {
+          type = "internal/xbacklight";
 
-        format = "<label>";
-        label = " %percentage%%";
-      };
-      "module/backlight" = {
-        type = "internal/backlight";
+          format = "<label>";
+          label = " %percentage%%";
+        };
+        "module/backlight" = {
+          type = "internal/backlight";
 
-        card = "intel_backlight";
+          card = "intel_backlight";
 
-        format = "<label>";
-        label = " %percentage%%";
-      };
-      "module/playerctl" = {
-        type = "custom/script";
-        exec = "playerctl --follow metadata --format '{{emoji(status)}} {{ artist }} - {{ title }}'";
-        tail = true;
-        click-left = "playerctl previous";
-        click-right = "playerctl next";
-        click-middle = "playerctl play-pause";
-      };
-      "module/cpu" = {
-        type = "internal/cpu";
-        interval = 2;
-        format-prefix = " ";
-        format-prefix-foreground = "\${colors.color2}";
-        label = "%percentage:2%%";
-      };
-      "module/memory" = {
-        type = "internal/memory";
-        interval = 2;
-        format-prefix = " ";
-        format-prefix-foreground = "\${colors.color3}";
-        label = "%percentage_used%%";
-      };
-      "module/eth" = {
-        type = "internal/network";
-        interface = "eno1";
-        interval = 3.0;
+          format = "<label>";
+          label = " %percentage%%";
+        };
+        "module/playerctl" = {
+          type = "custom/script";
+          exec = "playerctl --follow metadata --format '{{emoji(status)}} {{ artist }} - {{ title }}'";
+          tail = true;
+          click-left = "playerctl previous";
+          click-right = "playerctl next";
+          click-middle = "playerctl play-pause";
+        };
+        "module/cpu" = {
+          type = "internal/cpu";
+          interval = 2;
+          format-prefix = " ";
+          format-prefix-foreground = "\${colors.color2}";
+          label = "%percentage:2%%";
+        };
+        "module/memory" = {
+          type = "internal/memory";
+          interval = 2;
+          format-prefix = " ";
+          format-prefix-foreground = "\${colors.color3}";
+          label = "%percentage_used%%";
+        };
+        "module/eth" = {
+          type = "internal/network";
+          interface = "eno1";
+          interval = 3.0;
 
-        format-connected-underline = "#55aa55";
-        format-connected-prefix = " ";
-        format-connected-prefix-foreground = "\${colors.foreground}";
-        label-connected = "%local_ip%";
+          format-connected-underline = "#55aa55";
+          format-connected-prefix = " ";
+          format-connected-prefix-foreground = "\${colors.foreground}";
+          label-connected = "%local_ip%";
 
-        format-disconnected = "";
-      };
-      "module/date" = {
-        type = "internal/date";
-        interval = 5;
+          format-disconnected = "";
+        };
+        "module/date" = {
+          type = "internal/date";
+          interval = 5;
 
-        date = "%a, %d %b %Y";
-        date-alt = "%Y-%m-%d";
-        time = "%H:%M";
-        time-alt = "%H:%M:%S";
+          date = "%a, %d %b %Y";
+          date-alt = "%Y-%m-%d";
+          time = "%H:%M";
+          time-alt = "%H:%M:%S";
 
-        format-prefix = " ";
-        format-prefix-foreground = "\${colors.color4}";
+          format-prefix = " ";
+          format-prefix-foreground = "\${colors.color4}";
 
-        label = "%date% - %time%";
-      };
-      "module/pulseaudio" = {
-        type = "internal/pulseaudio";
+          label = "%date% - %time%";
+        };
+        "module/pulseaudio" = {
+          type = "internal/pulseaudio";
 
-        format-volume = "<label-volume> <bar-volume>";
-        label-volume = "";
-        label-volume-foreground = "\${root.foreground}";
+          format-volume = "<label-volume> <bar-volume>";
+          label-volume = "";
+          label-volume-foreground = "\${root.foreground}";
 
-        format-muted-prefix = " ";
-        format-muted-foreground = "\${root.foreground}";
-        label-muted = "sound muted";
+          format-muted-prefix = " ";
+          format-muted-foreground = "\${root.foreground}";
+          label-muted = "sound muted";
 
-        bar-volume-width = 10;
-        bar-volume-foreground = [
-          "#55aa55"
-          "#55aa55"
-          "#55aa55"
-          "#55aa55"
-          "#55aa55"
-          "#f5a70a"
-          "#ff5555"
-        ];
-        bar-volume-gradient = false;
-        bar-volume-indicator = "|";
-        bar-volume-fill = "─";
-        bar-volume-empty = "─";
-        bar-volume-empty-foreground = "\${colors.foreground-alt}";
-      };
-      "module/battery" = {
-        type = "internal/battery";
-        battery = "BAT0";
-        adapter = "ACAD";
-        full-at = 98;
+          bar-volume-width = 10;
+          bar-volume-foreground = [
+            "#55aa55"
+            "#55aa55"
+            "#55aa55"
+            "#55aa55"
+            "#55aa55"
+            "#f5a70a"
+            "#ff5555"
+          ];
+          bar-volume-gradient = false;
+          bar-volume-indicator = "|";
+          bar-volume-fill = "─";
+          bar-volume-empty = "─";
+          bar-volume-empty-foreground = "\${colors.foreground-alt}";
+        };
+        "module/battery" = {
+          type = "internal/battery";
+          battery = "BAT0";
+          adapter = "ACAD";
+          full-at = 98;
 
-        format-charging = "<label-charging>";
-        label-charging = " %percentage%%";
+          format-charging = "<label-charging>";
+          label-charging = " %percentage%%";
 
-        format-discharging = "<ramp-capacity> <label-discharging>";
+          format-discharging = "<ramp-capacity> <label-discharging>";
 
-        format-full-prefix = " ";
-        format-full-prefix-foreground = "\${colors.color6}";
+          format-full-prefix = " ";
+          format-full-prefix-foreground = "\${colors.color6}";
 
-        ramp-capacity = [ "" "" "" "" "" ];
-        ramp-capacity-foreground = "\${colors.color6}";
-      };
-      "module/powermenu" = {
-        type = "custom/menu";
+          ramp-capacity = [ "" "" "" "" "" ];
+          ramp-capacity-foreground = "\${colors.color6}";
+        };
+        "module/powermenu" = {
+          type = "custom/menu";
 
-        expand-right = true;
+          expand-right = true;
 
-        format-spacing = 1;
+          format-spacing = 1;
 
-        label-open = "";
-        label-open-foreground = "\${colors.color1}";
-        label-close = " cancel";
-        label-close-foreground = "\${colors.color1}";
-        label-separator = "|";
-        label-separator-foreground = "\${colors.foreground-alt}";
+          label-open = "";
+          label-open-foreground = "\${colors.color1}";
+          label-close = " cancel";
+          label-close-foreground = "\${colors.color1}";
+          label-separator = "|";
+          label-separator-foreground = "\${colors.foreground-alt}";
 
-        menu-0-0 = "log out";
-        menu-0-0-exec = "i3-msg exit";
-        menu-0-1 = "reboot";
-        menu-0-1-exec = "systemctl reboot";
-        menu-0-2 = "power off";
-        menu-0-2-exec = "systemctl poweroff";
+          menu-0-0 = "log out";
+          menu-0-0-exec = "i3-msg exit";
+          menu-0-1 = "reboot";
+          menu-0-1-exec = "systemctl reboot";
+          menu-0-2 = "power off";
+          menu-0-2-exec = "systemctl poweroff";
+        };
       };
     };
   };
 }
-  polybar.override or { }
