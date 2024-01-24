92f941d40b4cf1a4466e0ca950b4a74ac2a80ee1
Vojtěch Balák <vojtech@balak.me>
2024-01-24 10:07:45 +0100

    Disable pcscd for atuin

--- hosts/atuin/nixos.nix
+++ hosts/atuin/nixos.nix
@@ -178,7 +178,7 @@
 
   # Yubikey
   services.udev.packages = [ pkgs.yubikey-personalization ];
-  services.pcscd.enable = true;
+  # services.pcscd.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
