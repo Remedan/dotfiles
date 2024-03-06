38d7b52fd1761d2cf1efae31f07cc608e96ded6e
Vojtěch Balák <vojtech@balak.me>
2024-03-06 10:47:40 +0100

    Better Waybar config for Sway

--- modules/user/waybar.nix
+++ modules/user/waybar.nix
@@ -10,11 +10,123 @@ in
   config = mkIf cfg.enable {
     programs.waybar = {
       enable = true;
+
+      style = ''
+        * {
+            /* `otf-font-awesome` is required to be installed for icons */
+            font-family: FontAwesome, Roboto, Helvetica, Arial, sans-serif;
+            font-size: 13px;
+        }
+
+        window#waybar {
+            background-color: rgba(43, 48, 59, 0.5);
+            border-bottom: 3px solid rgba(100, 114, 125, 0.5);
+            color: #ffffff;
+            transition-property: background-color;
+            transition-duration: .5s;
+        }
+
+        window#waybar.hidden {
+            opacity: 0.2;
+        }
+
+        /*
+        window#waybar.empty {
+            background-color: transparent;
+        }
+        window#waybar.solo {
+            background-color: #FFFFFF;
+        }
+        */
+
+        window#waybar.termite {
+            background-color: #3F3F3F;
+        }
+
+        window#waybar.chromium {
+            background-color: #000000;
+            border: none;
+        }
+
+        button {
+            /* Use box-shadow instead of border so the text isn't offset */
+            box-shadow: inset 0 -3px transparent;
+            /* Avoid rounded borders under each button name */
+            border: none;
+            border-radius: 0;
+        }
+
+        /* https://github.com/Alexays/Waybar/wiki/FAQ#the-workspace-buttons-have-a-strange-hover-effect */
+        button:hover {
+            background: inherit;
+            box-shadow: inset 0 -3px #ffffff;
+        }
+
+        #workspaces button {
+            padding: 0 5px;
+            background-color: transparent;
+            color: #ffffff;
+        }
+
+        #workspaces button:hover {
+            background: rgba(0, 0, 0, 0.2);
+        }
+
+        #workspaces button.focused {
+            background-color: #64727D;
+            box-shadow: inset 0 -3px #ffffff;
+        }
+
+        #workspaces button.urgent {
+            background-color: #eb4d4b;
+        }
+
+        #mode {
+            background-color: #64727D;
+            box-shadow: inset 0 -3px #ffffff;
+        }
+
+        #clock,
+        #battery,
+        #cpu,
+        #memory,
+        #disk,
+        #temperature,
+        #backlight,
+        #network,
+        #pulseaudio,
+        #wireplumber,
+        #custom-media,
+        #tray,
+        #mode,
+        #idle_inhibitor,
+        #scratchpad,
+        #power-profiles-daemon,
+        #mpd {
+            padding: 0 10px;
+            color: #ffffff;
+        }
+
+        #window,
+        #workspaces {
+            margin: 0 4px;
+        }
+
+        /* If workspaces is the leftmost module, omit left margin */
+        .modules-left > widget:first-child > #workspaces {
+            margin-left: 0;
+        }
+
+        /* If workspaces is the rightmost module, omit right margin */
+        .modules-right > widget:last-child > #workspaces {
+            margin-right: 0;
+        }
+      '';
+
       settings = {
         mainBar = {
           modules-left = [
             "sway/workspaces"
-            "hyprland/workspaces"
             "pulseaudio"
             "sway/mode"
           ];
@@ -22,7 +134,7 @@ in
             "clock"
           ];
           modules-right = [
-            "hyprland/language"
+            "sway/language"
             "disk"
             "cpu"
             "memory"
@@ -30,6 +142,49 @@ in
             "battery"
             "tray"
           ];
+
+          pulseaudio = {
+            format = "{icon}  {volume}%";
+            format-bluetooth = "{icon} {volume}%";
+            format-muted = "";
+            format-icons = {
+              headphone = "";
+              hands-free = "";
+              headset = "";
+              phone = "";
+              portable = "";
+              car = "";
+              default = [ "" "" ];
+            };
+            on-click = "pavucontrol";
+          };
+
+          "sway/language" = {
+            format = "{}";
+            on-click = "swaymsg input type:keyboard xkb_switch_layout next";
+          };
+
+          disk = {
+            format = "/ {percentage_used}%";
+            path = "/";
+          };
+
+          cpu = {
+            format = "  {}%";
+          };
+
+          memory = {
+            format = "  {}%";
+          };
+
+          backlight = {
+            format = "  {}%";
+          };
+
+          battery = {
+            format = "{icon}  {capacity}%";
+            format-icons = [ "" "" "" "" ""];
+          };
         };
       };
     };
