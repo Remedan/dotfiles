8c3eeaf9590f998838ebbd274dfbad5d0659a895
Vojtěch Balák <vojtech@balak.me>
2024-05-21 17:02:34 +0200

    Add a few startup applications to weatherwax

--- hosts/weatherwax/user.nix
+++ hosts/weatherwax/user.nix
@@ -14,6 +14,13 @@ home-manager.lib.homeManagerConfiguration {
           enable = true;
           idleLock = false;
           nvidia = true;
+          startup = [
+            "solaar -w hide"
+            "[workspace 1 silent] firefox"
+            "[workspace 2 silent] thunderbird"
+            "[workspace 8 silent] steam"
+            "[workspace 9 silent] spotify"
+          ];
           workspaces = [
             "1, monitor:DP-1, default:true"
             "2, monitor:DP-1"
