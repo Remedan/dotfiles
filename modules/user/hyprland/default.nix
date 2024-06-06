3cb8afc3f8977025f966ba45f41f188ca4400b55
Vojtěch Balák <vojtech@balak.me>
2024-06-06 15:25:54 +0200

    Remove unneccessary variable from hyrpland

--- modules/user/hyprland/default.nix
+++ modules/user/hyprland/default.nix
@@ -160,7 +160,6 @@ in
             "XDG_SESSION_TYPE,wayland"
             "GBM_BACKEND,nvidia-drm"
             "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-            "WLR_NO_HARDWARE_CURSORS,1"
           ];
 
           exec-once = [
