5b4e10a74c87f6f972b2654a798fb9e5a864b29b
Vojtěch Balák <vojtech@balak.me>
2024-06-18 12:07:51 +0200

    Switch swaync to a home-manager module

--- modules/user/swaync.nix
+++ modules/user/swaync.nix
@@ -9,36 +9,21 @@ in
     enable = mkEnableOption "Sway Notification Center";
   };
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      swaynotificationcenter
-    ];
-    xdg.configFile."swaync/config.json".text = builtins.toJSON {
-      positionX = "center";
-      positionY = "top";
-      control-center-margin-top = 10;
-      control-center-margin-bottom = 10;
-      control-center-margin-right = 10;
-      control-center-margin-left = 10;
-      widgets = [
-        "inhibitors"
-        "title"
-        "dnd"
-        "notifications"
-      ];
-    };
-    systemd.user.services.swaync = {
-      Unit = {
-        Description = "Sway Notification Center";
-        After = [ "graphical-session-pre.target" ];
-        PartOf = [ "graphical-session.target" ];
-      };
-
-      Install = { WantedBy = [ "graphical-session.target" ]; };
-
-      Service = {
-        ExecStart = "${pkgs.swaynotificationcenter}/bin/swaync";
-        Restart = "always";
-        RestartSec = 3;
+    services.swaync = {
+      enable = true;
+      settings = {
+        positionX = "center";
+        positionY = "top";
+        control-center-margin-top = 10;
+        control-center-margin-bottom = 10;
+        control-center-margin-right = 10;
+        control-center-margin-left = 10;
+        widgets = [
+            "inhibitors"
+            "title"
+            "dnd"
+            "notifications"
+        ];
       };
     };
   };
