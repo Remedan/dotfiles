df65e07e8e6518c26c09f38aba19ec55203817a5
Vojtěch Balák <vojtech@balak.me>
2024-07-05 13:14:11 +0200

    Add some more Gnome options

--- modules/user/gnome.nix
+++ modules/user/gnome.nix
@@ -12,8 +12,22 @@ in
       gnome.gnome-tweaks
     ];
     dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+      "org/gnome/desktop/peripherals/touchpad" = {
+        tap-to-click = true;
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        focus-mode = "mouse";
+      };
       "org/gnome/mutter" = {
+        dynamic-workspaces = true;
+        edge-tiling = true;
         experimental-features = [ "scale-monitor-framebuffer" ];
+      };
+      "org/gnome/shell/app-switcher" = {
+        current-workspace-only = true;
       };
     };
   };
