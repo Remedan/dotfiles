baf43df42ae5f3a55a2a82768944d54c362fcf88
Vojtěch Balák <vojtech@balak.me>
2024-07-09 14:52:24 +0200

    Fix gnome shortcuts

--- modules/user/gnome.nix
+++ modules/user/gnome.nix
@@ -29,6 +29,7 @@ in
       "org/gnome/settings-daemon/plugins/media-keys" = {
         custom-keybindings = [
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
         ];
       };
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
