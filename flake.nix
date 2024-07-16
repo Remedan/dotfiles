3105ea6cddcee9f8524bf8db54dc54ed384a9de2
Vojtěch Balák <vojtech@balak.me>
2024-07-16 22:41:10 +0200

    Restrukture flake.nix a bit

--- flake.nix
+++ flake.nix
@@ -17,58 +17,51 @@
     {
       formatter.${system} = pkgs.nixpkgs-fmt;
 
-      nixosConfigurations = {
-        weatherwax = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/weatherwax/system.nix
-          ] ++ import ./modules/system;
-        };
-
-        rincewind = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/rincewind/system.nix
-            nixos-hardware.nixosModules.lenovo-thinkpad-x1-yoga
-          ] ++ import ./modules/system;
-        };
-
-        atuin = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/atuin/system.nix
-            ./secrets/atuin-system.nix
-            nixos-hardware.nixosModules.dell-latitude-5520
-          ] ++ import ./modules/system;
-        };
+      nixosConfigurations.weatherwax = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/weatherwax/system.nix
+        ] ++ import ./modules/system;
+      };
+      homeConfigurations."remedan@weatherwax" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          (import ./hosts/weatherwax/user.nix)
+          (import ./secrets/weatherwax-user.nix)
+          nix-flatpak.homeManagerModules.nix-flatpak
+        ] ++ import ./modules/user;
       };
 
-      homeConfigurations = {
-        "remedan@weatherwax" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            (import ./hosts/weatherwax/user.nix)
-            (import ./secrets/weatherwax-user.nix)
-            nix-flatpak.homeManagerModules.nix-flatpak
-          ] ++ import ./modules/user;
-        };
+      nixosConfigurations.rincewind = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/rincewind/system.nix
+          nixos-hardware.nixosModules.lenovo-thinkpad-x1-yoga
+        ] ++ import ./modules/system;
+      };
+      homeConfigurations."remedan@rincewind" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          (import ./hosts/rincewind/user.nix)
+          nix-flatpak.homeManagerModules.nix-flatpak
+        ] ++ import ./modules/user;
+      };
 
-        "remedan@rincewind" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            (import ./hosts/rincewind/user.nix)
-            nix-flatpak.homeManagerModules.nix-flatpak
-          ] ++ import ./modules/user;
-        };
-
-        "vojta@atuin" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            (import ./hosts/atuin/user.nix)
-            (import ./secrets/atuin-user.nix)
-            nix-flatpak.homeManagerModules.nix-flatpak
-          ] ++ import ./modules/user;
-        };
+      nixosConfigurations.atuin = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/atuin/system.nix
+          ./secrets/atuin-system.nix
+          nixos-hardware.nixosModules.dell-latitude-5520
+        ] ++ import ./modules/system;
+      };
+      homeConfigurations."vojta@atuin" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          (import ./hosts/atuin/user.nix)
+          (import ./secrets/atuin-user.nix)
+          nix-flatpak.homeManagerModules.nix-flatpak
+        ] ++ import ./modules/user;
       };
     };
 }
