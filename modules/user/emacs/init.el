eabbcd0405b57699321533154b66d9fc3dde9ad7
Vojtěch Balák <vojtech@balak.me>
2024-09-17 10:45:36 +0200

    Change treemacs bindings

--- modules/user/emacs/init.el
+++ modules/user/emacs/init.el
@@ -256,12 +256,13 @@
   :bind
   (:map global-map
         ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t d"   . treemacs-select-directory)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
+        ("C-c t 1"   . treemacs-delete-other-windows)
+        ("C-c t t"   . treemacs)
+        ("C-c t p"   . treemacs-add-and-display-current-project-exclusively)
+        ("C-c t d"   . treemacs-select-directory)
+        ("C-c t B"   . treemacs-bookmark)
+        ("C-c t C-t" . treemacs-find-file)
+        ("C-c t M-t" . treemacs-find-tag)))
 
 (use-package treemacs-evil
   :after (treemacs evil))
