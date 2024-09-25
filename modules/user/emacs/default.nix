156aa09e19577f6ba532524f60e21b7f666827b4
Vojtěch Balák <vojtech@balak.me>
2024-09-25 19:29:45 +0200

    Update Emacs to version 30

--- modules/user/emacs/default.nix
+++ modules/user/emacs/default.nix
@@ -27,7 +27,7 @@ in
     };
     programs.emacs = {
       enable = true;
-      package = pkgs.emacs29-pgtk;
+      package = pkgs.emacs30-pgtk;
       extraPackages = epkgs: with epkgs; [
         vterm
       ];
