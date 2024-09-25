7ba3a5a571bd21b58c7b8819aba4bb7440a35cd8
Vojtěch Balák <vojtech@balak.me>
2024-09-25 14:05:26 +0200

    Run nix fmt

--- modules/system/common.nix
+++ modules/system/common.nix
@@ -52,7 +52,7 @@ in
 
     # Allow KDEConnect
     networking.firewall = rec {
-      allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+      allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
       allowedUDPPortRanges = allowedTCPPortRanges;
     };
 
