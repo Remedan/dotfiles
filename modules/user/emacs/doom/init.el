97258c99dd2df90cf07d60bf1326195e532a9fac
Vojtěch Balák <vojtech@balak.me>
2025-02-05 17:21:56 +0100

    Enable treemacs

--- modules/user/emacs/doom/init.el
+++ modules/user/emacs/doom/init.el
@@ -44,7 +44,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
