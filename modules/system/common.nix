7c5664c559b959f2c2e8d1cb01c9361a4cdd3360
Vojtěch Balák <vojtech@balak.me>
2025-02-10 17:27:21 +0100

    Remove Open WebUI

--- modules/system/common.nix
+++ modules/system/common.nix
@@ -275,14 +275,6 @@ in
       enable = true;
       acceleration = mkIf config.system-modules.nvidia.enable "cuda";
     };
-    services.open-webui = {
-      enable = true;
-      environment = {
-        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
-        # Disable authentication
-        WEBUI_AUTH = "False";
-      };
-    };
 
     # Trezor
     services.trezord.enable = true;
