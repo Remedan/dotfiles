9a4147a82615d0213c911a4678558260eafcd2c9
Vojtěch Balák <vojtech@balak.me>
2025-02-24 17:31:56 +0100

    Actually show trailing whitespace in Emacs

--- modules/user/emacs/doom/config.el
+++ modules/user/emacs/doom/config.el
@@ -78,7 +78,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq show-trailing-whitespace t)
+(setq-default show-trailing-whitespace t)
 
 (use-package! nix-mode
   :mode "\\.nix\\'")
